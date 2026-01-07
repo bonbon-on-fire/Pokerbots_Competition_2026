aeefb3956a941ba0c2bdd8c3d4d9a34e734725d6
cindwei <cindywei@mit.edu>
2026-01-07 15:20:32 -0500

    Revert "max raise every time"
    This reverts commit e11ff041d0a5d1ab9710b5ef2b898c1e01bbc638.
    
    yes

--- python_skeleton/skeleton/bot.py
+++ python_skeleton/skeleton/bot.py
@@ -1,7 +1,7 @@
 '''
 This file contains the base class that you should implement for your pokerbot.
 '''
-from skeleton.actions import FoldAction, CallAction, CheckAction, RaiseAction, DiscardAction
+
 
 class Bot():
     '''
@@ -20,7 +20,7 @@ class Bot():
         Returns:
         Nothing.
         '''
-        return
+        raise NotImplementedError('handle_new_round')
 
     def handle_round_over(self, game_state, terminal_state, active):
         '''
@@ -34,7 +34,7 @@ class Bot():
         Returns:
         Nothing.
         '''
-        return
+        raise NotImplementedError('handle_round_over')
 
     def get_action(self, game_state, round_state, active):
         '''
@@ -49,12 +49,4 @@ class Bot():
         Returns:
         Your action.
         '''
-        legal_actions=round_state.legal_actions()
-        if DiscardAction in legal_actions:
-            return DiscardAction(legal_actions[DiscardAction][0])  # discard the first legal card
-        if RaiseAction in legal_actions:
-            max_raise=round_state.raise_bounds()[1]
-            return RaiseAction(legal_actions[RaiseAction][max_raise])  # minimum raise
-        elif CheckAction in legal_actions:
-            return CheckAction()
-        return CallAction()  
+        raise NotImplementedError('get_action')
