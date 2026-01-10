a94dd12bba237198f1bee774591ec4ae6421270c
angelika <amwangpanda@gmail.com>
2026-01-10 18:00:29 -0500

    rename bot as all_in

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # DO NOT REMOVE OR RENAME THIS FILE
 PLAYER_1_NAME = "A"
 # Change this to './player_chatbot' to interact with your own bot!
-PLAYER_1_PATH = "./our_bot"
+PLAYER_1_PATH = "./all_in_bot"
 # NO TRAILING SLASHES ARE ALLOWED IN PATHS
 PLAYER_2_NAME = "B"
 PLAYER_2_PATH = "./opponent_bot"
